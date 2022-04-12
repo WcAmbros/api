88b3e76757caa8ece4b51ee4daf9f48f67419f8f
stas <zahs88@yandex.ru>
2022-04-12 21:51:57 +0300

    fix

--- .k8s/k8s.sh
+++ .k8s/k8s.sh
@@ -49,6 +49,7 @@ DIR=$(dirname $0)
 
 source $DIR/../.env
 export PROJECT_PORT=$PORT
+export PROJECT_VERSION=$(cat $DIR/../package.json | jq -r '.version')
 export PROJECT_NAME=$CI_ENVIRONMENT_SLUG-$CI_PROJECT_NAME
 export PROJECT_CONFIG_MAP="$PROJECT_NAME"
 
